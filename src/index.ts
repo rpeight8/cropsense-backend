b9b3469a9720843c8666ac2caefc69db43440029
RPE8 <vitali.shuman96@gmail.com>
2023-07-02 14:04:49 +0300

    Add morgan and cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import app from "./server";
 import router from "./router";
 
-app.use("/api", router);
-
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
 import express from "express";
 import router from "./router";
+import cors from "cors";
+import logger from "./middlewares/logger";
+
+const environment = process.env.NODE_ENV || "development";
 
 const app = express();
 
+if (environment === "development") {
+  app.use(cors());
+}
+
+app.use(logger);
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", router);
 app.use(router);
 
 export default app;
